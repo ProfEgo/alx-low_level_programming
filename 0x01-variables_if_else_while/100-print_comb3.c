1c5db5cd51f46013287dd4d3295b43240ca862ea
ProfEgo <emmanuelgospel11@.com>
2022-04-10 15:36:00 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,16 @@ int number_left;
 int number_right;
 
 
-for (number_left = 44; number_right <= 79; number_left++)
+for (number_left = 44; number_right <= 88; number_left++)
 {
-for (number_right = number_left + 1 ; number_right <= 79; number_right++)
+for (number_right = number_left + 1 ; number_right <= 88; number_right++)
 {
 
 putchar(number_left);
 putchar(number_right);
 
 
-if ((number_left == 58) && (number_right == 79))
+if ((number_left == 58) && (number_right == 88))
 {
 break;
 }
