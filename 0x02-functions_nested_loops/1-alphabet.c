8ff60edc7de7e3c713b637fa90e32ebc80e958bd
ProfEgo <emmanuelgospel11@.com>
2022-04-12 16:24:47 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: _putchar print lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char cm;
+
+
+for (cm = 'a'; cm <= 'z'; cm++)
+{
+_putchar(cm);
+}
+
+
+_putchar('\n');
+
+
+}
