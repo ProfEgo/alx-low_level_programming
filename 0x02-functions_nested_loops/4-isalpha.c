5e6cb1834307622bc199372658402c720a8bddbe
ProfEgo <emmanuelgospel11@.com>
2022-04-12 18:34:24 -0700

    select-alph

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: Entry value
+ * Description: _isalpha print lowercase alphabet
+ * Descript: intergral 'c'
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else if (c > 'A' && c 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
