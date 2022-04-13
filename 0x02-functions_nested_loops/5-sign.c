30686cb2a5736a0bde8e7a0d04dfc5ed52554c33
ProfEgo <emmanuelgospel11@.com>
2022-04-12 19:43:16 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,16 @@ int print_sign(int n)
 
 {
 
-if (n > 0)
+if (n > '0')
 {
 return (1);
 _putchar('+');
-_putchar('\n');
 }
 
 else if (n == 0)
 {
 return (0);
-_putchar(0);
+_putchar('0');
 _putchar('\n');
 }
 
