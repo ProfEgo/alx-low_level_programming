2b9e4460bad39840fd07b94596e7ac08f1866895
ProfEgo <emmanuelgospel11@.com>
2022-04-13 08:51:13 -0700

    print-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	if (n < 98)
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -20,14 +20,14 @@ void print_to_98(int n)
 	else if (n == 98)
 	{
 		for (n = n; n >= 98; n--)
-		_putchar(n);
+		_putchar(n + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
 
 	else
 	{
-		_putchar(n);
+		_putchar(n + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
