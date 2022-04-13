3e3e919d2d8dbe8904b02d48f73f8116c4c0c521
ProfEgo <emmanuelgospel11@.com>
2022-04-13 12:57:21 -0700

    24_hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,7 +20,7 @@ for (num2 = 0; num2 <= 2; num2++)
 {
 	for (num3 = 0; num3 <= 3; num3++)
 	{
-		for (num4 =0; num4 <= 5; num4++)
+		for (num4 = 0; num4 <= 5; num4++)
 		{
 			for (num5 = 0; num5 <= 9; num5++)
 			{
