77c8303c72c0fa28c1eefb1cee633d19428e7713
ProfEgo <emmanuelgospel11@.com>
2022-04-13 04:38:57 -0700

    6_abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,29 +2,28 @@
 #include <stdio.h>
 
 /**
- * _abs - Absolute value
- * @r: Entry value
- * Description: _islower print lowercase alphabet
- * Descript: intergral 'c'
- * Return: 0
+ * _abs - value absolute
+ *
+ *@n: The int to print
+ * Return: Always 0.
  */
 
-int _abs(int r)
+int _abs(int n)
 {
 
-if (r < 0)
+if (n < 0)
 {
-_putchar(r * (-1));
+return (n * (-1));
 }
 
-else if (r == 0)
+else if (n == 0)
 {
-_putchar(0);
+return (0);
 }
 
 else
 {
-_putchar(r);
+return (n);
 }
-return (r);
+
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,21 +9,20 @@
 int print_last_digit(int n)
 {
 
+if (n < 0)
+{
+n = (-1 * (n % 10));
+_putchar(n + '0');
+_putchar('\n');
+return (n);
+}
 
-	if (n < 0)
-	{
-		n = (-1 * (n % 10));
-		_putchar(n + '0');
-		_putchar('\n');
-		return (n);
-	}
-
-	else
-	{
-		n = n % 10;
-		_putchar(n + '0');
-		_putchar('\n');
-		return (n);
-	}	
+else
+{
+n = n % 10;
+_putchar(n + '0');
+_putchar('\n');
+return (n);
+}	
 
 }
