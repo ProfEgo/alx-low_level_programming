c76c9f298338ce5b7c56d56ce42634d52fa455fe
ProfEgo <emmanuelgospel11@.com>
2022-04-12 17:50:31 -0700

    lower-alp

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: _putchar print lowercase alphabet
+ * c - int
+ * Descriptio: hold an intigal
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+
+int c;
+
+if (c < 'a' && c > 'z')
+{
+_putchar(0);
+}
+
+else
+{
+_putchar(1);
+}
+
+}
