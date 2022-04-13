e331f11009cef0c1989c414382d4eab197d6fe39
ProfEgo <emmanuelgospel11@.com>
2022-04-13 12:48:37 -0700

    print-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,6 +6,8 @@
  * Return: Always 0.
  */
 void print_to_98(int n)
+{
+
 if (n < 98)
 {
 for (n = n; n < 98; n++)
@@ -19,4 +21,5 @@ for (n = n; n > 98; n--)
 printf("%d, ", n);
 printf("%d", 98);
 }
+
 }
