f15fad9b29d850fcd9b862345f52bcf472d22dc7
ProfEgo <emmanuelgospel11@.com>
2022-04-13 13:03:52 -0700

    24_hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,22 +18,21 @@ int num5;
 
 for (num2 = 0; num2 <= 2; num2++)
 {
-	for (num3 = 0; num3 <= 3; num3++)
-	{
-		for (num4 = 0; num4 <= 5; num4++)
-		{
-			for (num5 = 0; num5 <= 9; num5++)
-			{
-			_putchar(num2 + '0');
-			_putchar(num3 + '0');
-			_putchar(':');
-			_putchar(num4 + '0');
-			_putchar(num5 + '0');
-			_putchar('\n');
-			}
-
-		}
-	}
+for (num3 = 0; num3 <= 3; num3++)
+{
+for (num4 = 0; num4 <= 5; num4++)
+{
+for (num5 = 0; num5 <= 9; num5++)
+{
+_putchar(num2 + '0');
+_putchar(num3 + '0');
+_putchar(':');
+_putchar(num4 + '0');
+_putchar(num5 + '0');
+_putchar('\n');
+}
+}
+}
 }
 
 }
