28a20b1da81cc187779a14c2c2532dc4ea38b990
ProfEgo <emmanuelgospel11@.com>
2022-04-13 07:19:15 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,37 +9,31 @@
 void jack_bauer(void)
 {
 
-int num1;
 int num2;
 int num3;
 int num4;
 int num5;
 
 
-for (num1 = 0; num1 <= 1; num1++)
-{
-	for (num2 = 0; num2 < 3; num2++)
-	{
-		for (num3 = 0; num3 < 4; num3++)
-		{
-			for (num4 = 0; num4 < 6; num4++)
-			{
-				for (num5 = 0; num5 < 9; num5++)
-				{
-				_putchar(num2 + '0');
-				_putchar(num3 + '0');
-				_putchar(':');
-				_putchar(num4 + '0');
-				_putchar(num5 + '0');
-				_putchar('\n');
-				}
 
+for (num2 = 0; num2 <= 2; num2++)
+{
+	for (num3 = 0; num3 <= 3; num3++)
+	{
+		for (num4 = 0; num4 <= 5; num4++)
+		{
+			for (num5 = 0; num5 <= 9; num5++)
+			{
+			_putchar(num2 + '0');
+			_putchar(num3 + '0');
+			_putchar(':');
+			_putchar(num4 + '0');
+			_putchar(num5 + '0');
+			_putchar('\n');
 			}
+
 		}
 	}
-
-
-_putchar('\n');
 }
 
 }
