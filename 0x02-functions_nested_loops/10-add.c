3c6447f9a4c6efb963df5a3917982d9b03456b18
ProfEgo <emmanuelgospel11@.com>
2022-04-13 05:34:51 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - value absolute
+ * @p: initialization of the addition of values
+ * @m: first int to print
+ * @n: second int to print
+ * Return: Always 0.
+ */
+
+int add(int m, int n)
+{
+int p;
+p = m + n;
+_putchar(p + '0');
+return (0);
+
+}
