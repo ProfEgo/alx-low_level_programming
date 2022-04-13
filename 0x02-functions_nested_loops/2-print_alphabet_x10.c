381cf62a1a32b3e27129a0c562fa3e34eed90578
ProfEgo <emmanuelgospel11@.com>
2022-04-12 17:06:52 -0700

    print-Alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: _putchar print lowercase alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char alphabet;
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar(i);
+	_putchar('\n');
+}
+
+}
