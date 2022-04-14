5075d784de2411671dd5735182b9241a0d082b99
ProfEgo <emmanuelgospel11@.com>
2022-04-13 17:53:16 -0700

    pos-neg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests function that
+ * @i: the int to be returned
+ *
+ * Description: To check positive & negative
+ * Return: 1 if positive. 0 otherwise
+ */
+
+void positive_or_negative(int i)
+{
+
+if (i > 0)
+{
+	printf("%d is positive\n", i);
+}
+
+else if (i == 0)
+{
+	printf("%d is positive\n", i);
+}
+
+else
+{
+	printf("%d is positive\n", i);
+}
+
+
+}
