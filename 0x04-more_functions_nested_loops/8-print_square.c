f9c068168d7db57feb2f7ff6c09faf79f7a57d72
ProfEgo <emmanuelgospel11@.com>
2022-04-15 02:42:24 -0700

    4_more-loop

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,15 +12,14 @@ int i;
 
 if (size >= 0)
 {
-for (i = 0; i <= size; i++)
+for (i = 0; i < size; i++)
 {
-
 int k;
-for (k = 0; k <= size; k++)
+for (k = 0; k < size; k++)
 {
 _putchar (35);
-}
 _putchar ('\n');
+}
 
 }
 
