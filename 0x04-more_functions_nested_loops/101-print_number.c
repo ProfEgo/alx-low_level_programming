c1825bae87c6b53b6b61b04b723e3a46b63d8ced
ProfEgo <emmanuelgospel11@.com>
2022-04-16 16:36:45 -0700

    print-numb

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,31 @@
 /**
  *print_number - that prints an integer
  *@n: print int
+ *
  *Return: Always 0.
  */
 void print_number(int n)
 {
-_putchar(n + '0');
+
+long m = n * -1;
+long c = m % 10;
+long p = m / 10;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(p + '0');
+		_putchar(c + '0');
+	}
+
+	if (n >= 0 || n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	if (n > 9)
+	{
+		_putchar(p + '0');
+		_putchar(c + '0');
+	}
+
 }
